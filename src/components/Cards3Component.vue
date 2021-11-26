8db8942c2846aae0e63d80adea73fd41aa6c4940
Andretreccia <andreabtreccia@gmail.com>
2021-11-26 13:37:03 +0100

    Transform Card3Component as a responsive and reusable component

---
+++ src/components/Cards3Component.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container pt-5">
+    <div class="row d-flex justify-content-center">
+      <div class="col-4" v-for="element in ArrayOne" :key="element.qua">
+        <div class="card text-center px-5 py-5">
+          <img
+            :src="require('../assets/images/' + element.img)"
+            alt=""
+            v-if="element.img"
+          />
+          <i :class="element.icon" class="mt-3 fs-3" v-if="element.icon"></i>
+          <h5 class="fw-bold fs-4 mt-4" v-if="element.title">
+            {{ element.title }}
+          </h5>
+          <span v-if="element.date">{{ element.date }}</span>
+          <p class="mt-3 mb-5" v-if="element.text">
+            {{ element.text }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    ArrayOne: Array,
+  },
+};
+</script>
+
+
+<style lang="scss">
+@import "../assets/scss/color.scss";
+.text {
+  width: 500px;
+  margin: auto;
+  margin-bottom: 4rem;
+}
+.row {
+  .card {
+    border: none;
+    i {
+      color: $contrast-color;
+    }
+  }
+}
+</style>
--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,16 +1,28 @@
 <template>
   <main id="site-main">
-    <ComponentOne :SectionOne="SectionOne" />
+    <div class="container-fluid text-center">
+      <div class="text text-center">
+        <h2 class="mb-4 fw-bold">
+          Meet The New Agency SEO Template From The Avada Team
+        </h2>
+        <p>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium.
+        </p>
+      </div>
+      <Cards3Component :ArrayOne="SectionOne" />
+    </div>
+    <img src="../assets/images/agency-seo-desk-front-1200x570.jpg" alt="" />
   </main>
 </template>
 
 <script>
-import ComponentOne from "./ComponentOne.vue";
+import Cards3Component from "./Cards3Component.vue";
 export default {
   props: {
     SectionOne: Array,
   },
-  components: { ComponentOne },
+  components: { Cards3Component },
 };
 </script>
 
